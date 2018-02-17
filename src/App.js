62b7aa63c28e880ff92907ca64ce96684b6358c1
jvke <hi@jvke.co>
2018-02-17 15:09:52 +1100

    Remove debug code

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import Info from './containers/Info';
 
 import './App.css';
 
-// chrome.webRequest.onCompleted.addListener(
-//   function(details) {
-//     let dns = {};
-//     var host = /^(?:(\w+):)?\/\/([^\/\?#]+)/.exec(details['url']);
-//     console.info(host);
-//     if (details['ip']) {
-//       if (dns[host[2]]) {
-//         if (dns[host[2]].includes(details['ip'])) {
-//           return 1;
-//         } else {
-//           dns[host[2]] = details['ip'] + ', ' + dns[host[2]];
-//           console.info(dns);
-//         }
-//       } else {
-//         console.info(dns);
-//         dns[host[2]] = details['ip'];
-//         console.info(dns);
-//       }
-//     }
-//   },
-//   { urls: ['<all_urls>'] },
-//   []
-// );
-
 const App = () => (
   <div className="app-container">
     <Background />
